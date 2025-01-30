ede8e953dc65e0084fafddad1c90784be1804883
Pawel Tulowiecki <ptulowiecki@griddynamics.com>
2025-01-30 22:16:01 +0100

    setup neovim

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -1,18 +1,25 @@
-sudo apt update
-
-sudo apt install -y neovim
-
 config_home=~/.config/nvim
 if ! [[ -d $config_home ]]; then
     echo "creating $config_home"
     mkdir $config_home
 fi
-curl -fLo ${config_home}/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl https://raw.githubusercontent.com/pawtul/dotfiles/master/nvimrc > $config_home/init.vim
+curl https://raw.githubusercontent.com/pawtul/dotfiles/master/init.lua > $config_home/init.lua
 pip install --upgrade pynvim
 
-nvim -c PlugInstall
-
 # setup my snippets
+if ! [[ -d ~/.vim ]]; then
+    mkdir ~/.vim
+fi
 git clone https://github.com/pawtul/codesnippets.git ~/.vim/snippets
+
+
+version=${NEOVIM_VERSION:-v0.10.4}
+if ! [[ -d ~/bin ]]; then
+    mkdir ~/bin
+fi
+wget "https://github.com/neovim/neovim/releases/download/${version}/nvim-linux-x86_64.tar.gz" -P /tmp/
+tar xf /tmp/nvim-linux-x86_64.tar.gz -C /tmp/
+cp /tmp/nvim-linux-x86_64/bin/nvim ~/bin
+
+
